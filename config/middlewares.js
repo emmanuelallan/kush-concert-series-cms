1c9f32ad0a493691ba9c73e089fc10d30d9f1e0f
emmanuelallan <imma.allan@gmail.com>
2024-09-20 22:42:32 +0300

    add cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,18 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      header: "*",
+      origin: ["http://localhost:3000"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
